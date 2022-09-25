7bde1bac93c6343d1450fcdffcd978571b7ff064
nikonovdmitiy <nikonov9094@gmail.com>
2022-09-25 22:59:43 +0500

    ДЗ

--- src/Car.java
+++ src/Car.java
@@ -4,16 +4,45 @@ public class Car {
     String model;
     double engineVolume;
     String color;
-    int productionYear;
+    Integer productionYear;
     String productionCountry;
 
-    Car(String brand, String model, int productionYear, String productionCountry, String color, double engineVolume) {
-        this.brand = brand;
-        this.model = model;
-        this.productionYear = productionYear;
-        this.productionCountry = productionCountry;
+    Car(String brand, String model, Integer productionYear, String productionCountry, String color, double engineVolume) {
+
+        if(brand==null){
+            this.brand = "default";
+        }else {
+            this.brand = brand;
+        }
+
+        if(model==null){
+            this.model = "default";
+        }else {
+            this.model = model;
+        }
+
+        if(productionYear==null){
+            this.productionYear = 2000;
+        }else {
+            this.productionYear = productionYear;
+        }
+
+        if(productionCountry==null){
+            this.productionCountry = "default";
+        }else {
+            this.productionCountry = productionCountry;
+        }
+
+        if(color==null){
+            this.color = "белый";
+        }
         this.color = color;
-        this.engineVolume = engineVolume;
+
+        if(engineVolume==null){
+            this.engineVolume = 1.5;
+        }else {
+            this.engineVolume = engineVolume;
+        }
     }
 
 
