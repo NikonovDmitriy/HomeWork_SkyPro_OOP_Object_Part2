dfe5a5cc4e7dcfede262a87deb3086d83d82550d
nikonovdmitiy <nikonov9094@gmail.com>
2022-10-21 01:02:35 +0500

    ДЗ

---
+++ src/products/Main.java
@@ -0,0 +1,30 @@
+package products;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var shopList = new ShopList();
+
+        shopList.add(Name.APPLE, 1.5, 11.2);
+        shopList.add(Name.ORANGE, 2.5, 13.4);
+        shopList.add(Name.GRAPE, 3.4, 15.6);
+
+        shopList.markPurchased(Name.APPLE);
+        shopList.show();
+
+        System.out.println();
+        shopList.remove(Name.APPLE);
+        shopList.show();
+
+
+        var recipesBook = new RecipesBook();
+
+        var recipe = new Recipe("первй рецепт");
+        recipe.addProduct(new Product(Name.APPLE, 1.2, 15.5));
+        recipe.addProduct(new Product(Name.GRAPE, 1.5, 16.7));
+        recipesBook.addRecipe(recipe);
+
+    }
+}
---
+++ src/products/Many.java
@@ -0,0 +1,25 @@
+package products;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Many {
+
+
+        public static Set<Integer> randomNumbers(){
+            Set<Integer> integers = new HashSet<>();
+            for (int i = 0; i < 20; i++) {
+                double random = Math.random() * 1000;
+                integers.add((int) random);
+            }
+            integers.removeIf(integer -> integer % 2 == 0);
+            return integers;
+        }
+
+    }
+
+
+
+
+
+
