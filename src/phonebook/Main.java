f54a21fb66113cae7f3fef2c9fbcc690cf3a5a50
nikonovdmitiy <nikonov9094@gmail.com>
2022-10-30 23:30:14 +0500

    ДЗ

--- src/phonebook/Main.java
+++ src/phonebook/Main.java
@@ -1,11 +1,32 @@
 package phonebook;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
 
+        task3();
         task1();
 
     }
+
+    private static void task3() {
+        Map<String, Integer> map = new HashMap<>();
+
+        String key = "123";
+        int value = 123;
+        add(key, value, map);
+    }
+
+    private static void add(String k, int v, Map<String, Integer> map) {
+        if (map.containsKey(k) && map.get(k).equals(v)) {
+            throw new IllegalArgumentException();
+        }
+        map.put(k, v);
+    }
+
+
 
     private static void task1() {
         var phoneBook = new PhoneBook();
